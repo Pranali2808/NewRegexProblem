4aee899fcc111c8c7ffe9b3de2dc8c93789c2331
pranali2808 <ranepranali1997@gmail.com>
2022-04-29 15:56:54 +0530

    check last name is valid or not

--- NewUserRegistrationProblem/Program.cs
+++ NewUserRegistrationProblem/Program.cs
@@ -16,6 +16,10 @@ namespace NewUserRegistrationProblem
             string firstName = Console.ReadLine();
             bool fNameResult = validateUser.ValidateFirstName(firstName);
             validateUser.PrintResult(fNameResult);
+            Console.WriteLine("Enter your last name: ");
+            string lastName = Console.ReadLine();
+            bool lNameResult = validateUser.ValidateLastName(lastName);
+            validateUser.PrintResult(lNameResult);
             Console.ReadLine();
         }
     }
